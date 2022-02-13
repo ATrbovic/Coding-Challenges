0ef8c87d8d787dd56f5a410244bc6867080176ab
Alex Trbovic <alextrbovic@outlook.com>
2022-02-13 12:08:05 -0500

    Updated solution

--- Codewars/which-are-in?.js
+++ Codewars/which-are-in?.js
@@ -1,27 +1,31 @@
 ///Given two arrays of strings a1 and a2 return a sorted array r 
 // in lexicographical order of the strings of a1 which are substrings of strings of a2.
 
-unction inArray(array1,array2){
-  const _match = []
+function inArray(array1, array2) {
+  var i, j, result = [];
 
-  for (let i = 0; i < array2.length; i++) {
-    for (let x = 0; x < array1.length; x++) {
-      const a1El = array1[x];
-      const a2El = array2[i];
-      if (a2El.includes(a1El)) {
-        _match.push(a1El)
+  outer: for (j = 0; j < array1.length; j++) {
+      for (i = 0; i < array2.length; i++) {
+          if (array2[i].includes(array1[j])) {
+              result.push(array1[j]);
+              continue outer;
+          }
       }
-    }
   }
-
-  return _match.filter((el, i, a) => a.indexOf(el) === i).sort((a, b) => {
-    const _a = a.toLowerCase();
-    const _b = b.toLowerCase();
-
-    if (_a < _b) {
-      return -1;
-    }
-
-    return 1;
-  })
+  return result.sort();
 }
+
+var Test = {
+  assertSimilar: function (a, b) {
+      console.log('result', a);
+      console.log('given', b);
+  }
+};
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"];
+a1 = ["xyz", "live", "strong"];
+Test.assertSimilar(inArray(a1, a2), ["live", "strong"]);
+a1 = ["live", "strong", "arp"];
+Test.assertSimilar(inArray(a1, a2), ["arp", "live", "strong"]);
+a1 = ["tarp", "mice", "bull"];
+Test.assertSimilar(inArray(a1, a2), []);
