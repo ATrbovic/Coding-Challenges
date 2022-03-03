c48e8c755968419c2af8f3c5da043775e409b69c
Alex Trbovic <alextrbovic@outlook.com>
2022-03-03 10:45:58 -0500

    Create Product-Of-Consecutive-Fib-Numbers.js

---
+++ Codewars/Product-Of-Consecutive-Fib-Numbers.js
@@ -0,0 +1,16 @@
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+// such as F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) 
+// and F(n+1) verifying F(n) * F(n+1) = prod.
+// Your function productFib takes an integer (prod) and returns an array:
+
+function productFib(prod){
+  var n = 0;
+  var nPlus = 1;  
+  while(n*nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [n, nPlus, n*nPlus===prod];
+}
